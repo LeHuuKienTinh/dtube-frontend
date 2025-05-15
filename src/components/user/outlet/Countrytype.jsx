f4054028d9514203dff05af4d7a64ab37a6fd417
LeHuuKienTinh <lehkientinh@dtu.edu.vn>
2025-05-16 02:03:10 +0700

    fixed2

--- src/components/user/outlet/Countrytype.jsx
+++ src/components/user/outlet/Countrytype.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Menu from '../../../view/user/Menu';
-import './countrytype.scss';
+import './Countrytype.scss';
 import axiosInstance from '../../../service/axiosInstance';
 
 const Countrytype = () => {
